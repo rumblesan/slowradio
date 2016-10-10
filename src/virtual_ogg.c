75e858e8c54f3f2b3933731f40b32ec2a0b648ab
Guy John <guy@rumblesan.com>
2016-10-10 21:34:40 +0100

    Update virtual ogg to write to ring buffer

--- src/virtual_ogg.c
+++ src/virtual_ogg.c
@@ -3,7 +3,10 @@
 
 #include "virtual_ogg.h"
 
+#include "filechunk.h"
+
 #include "bclib/dbg.h"
+#include "bclib/ringbuffer.h"
 
 SF_VIRTUAL_IO *virtual_ogg_create() {
   SF_VIRTUAL_IO *sfvirt = malloc(sizeof(SF_VIRTUAL_IO));
@@ -32,8 +35,16 @@ sf_count_t virt_read(void *ptr, sf_count_t count, void *user_data) {
   printf("Called file read\n");
   return 0;
 }
-sf_count_t virt_write(const void *ptr, sf_count_t count, void *user_data) {
+sf_count_t virt_write(const void *ptr, sf_count_t count, void *_rb) {
+  // TODO - More error handling here
   printf("Called file write\n");
+  RingBuffer *rb = _rb;
+  log_info("**** rb pointer: %p", rb);
+  unsigned char *data = malloc(count * sizeof(unsigned char *));
+  memcpy (data, ptr, (size_t) count) ;
+  FileChunk *fc = file_chunk_create(data, count);
+  int pos = rb_push(rb, fc);
+  log_info("pos is %d", pos);
   return 0;
 }
 sf_count_t virt_tell(void *user_data) {
