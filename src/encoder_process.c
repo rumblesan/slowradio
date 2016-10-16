1e1285a199444a46431db452df5e6a009d96106d
Guy John <guy@rumblesan.com>
2016-10-16 16:23:32 +0100

    Cleaning up encoder process

--- src/encoder_process.c
+++ src/encoder_process.c
@@ -9,6 +9,8 @@
 #include "filechunk.h"
 #include "messages.h"
 
+#include "util.h"
+
 #include "bclib/dbg.h"
 #include "bclib/ringbuffer.h"
 
@@ -16,17 +18,17 @@ EncoderProcessState *encoder_process_state_create(int channels,
                                                   int samplerate,
                                                   int format,
                                                   int usleep_time,
-                                                  RingBuffer *msg_in,
-                                                  RingBuffer *msg_out) {
+                                                  RingBuffer *pipe_in,
+                                                  RingBuffer *pipe_out) {
 
   EncoderProcessState *state = malloc(sizeof(EncoderProcessState));
   check_mem(state);
 
-  check(msg_in != NULL, "Invalid msg in buffer passed");
-  state->pipe_in = msg_in;
+  check(pipe_in != NULL, "Invalid msg in buffer passed");
+  state->pipe_in = pipe_in;
 
-  check(msg_out != NULL, "Invalid msg out buffer passed");
-  state->pipe_out = msg_out;
+  check(pipe_out != NULL, "Invalid msg out buffer passed");
+  state->pipe_out = pipe_out;
 
   state->channels    = channels;
   state->samplerate  = samplerate;
@@ -43,30 +45,34 @@ void encoder_process_state_destroy(EncoderProcessState *state) {
 }
 
 EncoderState waiting_for_file_state(EncoderProcessState *info, OggEncoderState *encoder, Message *input_msg) {
-  TrackInfo *tinfo    = NULL;
-  FileChunk *headers  = NULL;
-  Message *output_msg = NULL;
 
   // TODO move this into info
   float quality       = 0.5;
 
   if (input_msg->type == NEWTRACK) {
     log_info("Encoder: New Track received");
-    tinfo = input_msg->payload;
-    log_info("New Track: %s - %s", bdata(tinfo->artist), bdata(tinfo->title));
-    message_destroy(input_msg);
 
+    log_info("Encoder: Creating new encoder");
     (*encoder) = *(ogg_encoder_state(info->channels, info->samplerate, quality));
-    set_headers(encoder);
-    headers = file_chunk_create();
-    check(headers != NULL, "Could not create headers file chunk");
 
+    TrackInfo *tinfo = input_msg->payload;
+    check(tinfo != NULL, "Could not get track info from message");
+
+    // TODO setting metadata should be part of setting headers
+    log_info("New Track: %s - %s", bdata(tinfo->artist), bdata(tinfo->title));
+    set_headers(encoder);
+
+    FileChunk *headers = file_chunk_create();
+    check(headers != NULL, "Could not create headers file chunk");
     write_headers(encoder, headers);
-    output_msg = file_chunk_message(headers);
+    Message *output_msg = file_chunk_message(headers);
     check(output_msg != NULL, "Could not create headers message");
 
     rb_push(info->pipe_out, output_msg);
+
+    message_destroy(input_msg);
     return ENCODINGFILE;
+
   } else {
     log_err("Encoder: Received message of type %s but waiting for new track", msg_type(input_msg));
     message_destroy(input_msg);
@@ -77,14 +83,14 @@ EncoderState waiting_for_file_state(EncoderProcessState *info, OggEncoderState *
 }
 
 EncoderState encoding_file_state(EncoderProcessState *info, OggEncoderState *encoder, Message *input_msg) {
-  AudioBuffer *audio    = NULL;
   FileChunk *audio_data = NULL;
   Message *output_msg   = NULL;
 
   if (input_msg->type == AUDIOBUFFER) {
-    audio = input_msg->payload;
+    AudioBuffer *audio = input_msg->payload;
     check(audio != NULL, "Received invalid audio");
     add_audio(encoder, audio);
+
     audio_data = file_chunk_create();
     check(audio_data != NULL, "Could not create audio data");
     write_audio(encoder, audio_data);
@@ -96,12 +102,15 @@ EncoderState encoding_file_state(EncoderProcessState *info, OggEncoderState *enc
       check(output_msg != NULL, "Could not create audio message");
       rb_push(info->pipe_out, output_msg);
     }
+
     message_destroy(input_msg);
     return ENCODINGFILE;
+
   } else if (input_msg->type == STREAMFINISHED) {
     log_info("Encoder: Stream Finished message received");
     message_destroy(input_msg);
     return CLOSINGSTREAM;
+
   } else if (input_msg->type == TRACKFINISHED) {
     log_info("Encoder: Track Finished message received");
 
@@ -109,7 +118,8 @@ EncoderState encoding_file_state(EncoderProcessState *info, OggEncoderState *enc
     file_finished(encoder);
     audio_data = file_chunk_create();
     check(audio_data != NULL, "Could not create audio data");
-    int oggfinished = 0;
+
+    bool oggfinished = false;
     while (!oggfinished) {
       oggfinished = write_audio(encoder, audio_data);
     }
@@ -139,32 +149,21 @@ EncoderState encoding_file_state(EncoderProcessState *info, OggEncoderState *enc
 void *start_encoder_process(void *_info) {
   EncoderProcessState *info = _info;
 
-  Message *input_msg = NULL;
-
   OggEncoderState encoder;
 
   EncoderState state = WAITINGFORFILE;
 
-  bool running = true;
-
   check(info != NULL, "Encoder: Invalid info data passed");
 
-  int startup_wait = 1;
-  while (true) {
-    if (!rb_empty(info->pipe_in)) {
-      log_info("Encoder: Audio available");
-      break;
-    } else {
-      log_info("Encoder: Waiting for input audio...");
-      sleep(startup_wait);
-    }
-  }
-  log_info("Encoder: Starting");
+  log_info("Encoder: Waiting for input");
+  check(wait_for_input(info->pipe_in, 1, 60), "Encoder: Could not get input in time");
 
+  log_info("Encoder: Starting");
+  bool running = true;
   while (running) {
 
-    if (!rb_full(info->pipe_out) && !rb_empty(info->pipe_in)) {
-      input_msg = rb_pop(info->pipe_in);
+    if (pipes_ready(info->pipe_in, info->pipe_out)) {
+      Message *input_msg = rb_pop(info->pipe_in);
       check(input_msg != NULL, "Encoder: Could not get input message");
 
       switch(state) {
