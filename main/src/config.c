509b14a086c6971bad498667acc73999314ab060
Guy John <guy@rumblesan.com>
2016-10-21 21:54:55 +0100

    Improve config error messages a bit

--- main/src/config.c
+++ main/src/config.c
@@ -26,13 +26,12 @@ RadioInputCfg *read_config(char *config_path) {
   config_init(cfg);
 
   check(config_read_file(cfg, config_path),
-        "%s:%d - %s", config_error_file(cfg),
-        config_error_line(cfg), config_error_text(cfg));
+        "Could not read config file - %s:%d - %s",
+        config_error_file(cfg), config_error_line(cfg), config_error_text(cfg));
 
   // Channels config
   check(config_lookup_int(cfg, "channels", &(radio_config->channels)),
-        "%s:%d - %s", config_error_file(cfg),
-        config_error_line(cfg), config_error_text(cfg));
+        "Could not read channels setting");
 
   // File Reader config
   config_setting_t *frsetting = config_lookup(cfg, "filereader");
@@ -40,7 +39,7 @@ RadioInputCfg *read_config(char *config_path) {
         config_setting_lookup_int(frsetting, "read_size", &(radio_config->filereader.read_size)) &&
         config_setting_lookup_bstring(frsetting, "pattern", &(radio_config->filereader.pattern)) &&
         config_setting_lookup_int(frsetting, "thread_sleep", &(radio_config->filereader.thread_sleep)),
-        "%s:%d - %s", config_error_file(cfg), config_error_line(cfg), config_error_text(cfg));
+        "Could not read filereader settings");
 
   // Stretcher config
   config_setting_t *strsetting = config_lookup(cfg, "stretcher");
@@ -48,7 +47,7 @@ RadioInputCfg *read_config(char *config_path) {
         config_setting_lookup_float(strsetting, "stretch", &(radio_config->stretcher.stretch)) &&
         config_setting_lookup_int(strsetting, "window_size", &(radio_config->stretcher.window_size)) &&
         config_setting_lookup_int(strsetting, "thread_sleep", &(radio_config->stretcher.thread_sleep)),
-        "%s:%d - %s", config_error_file(cfg), config_error_line(cfg), config_error_text(cfg));
+        "Could not read stretcher settings");
 
   // Encoder config
   config_setting_t *encsetting = config_lookup(cfg, "encoder");
@@ -56,7 +55,7 @@ RadioInputCfg *read_config(char *config_path) {
         config_setting_lookup_int(encsetting, "samplerate", &(radio_config->encoder.samplerate)) &&
         config_setting_lookup_int(encsetting, "thread_sleep", &(radio_config->encoder.thread_sleep)) &&
         config_setting_lookup_float(encsetting, "quality", &(radio_config->encoder.quality)),
-        "%s:%d - %s", config_error_file(cfg), config_error_line(cfg), config_error_text(cfg));
+        "Could not read encoder settings");
 
   // Broadcast config
   config_setting_t *broadcastsetting = config_lookup(cfg, "broadcast");
@@ -70,7 +69,7 @@ RadioInputCfg *read_config(char *config_path) {
         config_setting_lookup_bstring(broadcastsetting, "description", &(radio_config->broadcast.description)) &&
         config_setting_lookup_bstring(broadcastsetting, "genre", &(radio_config->broadcast.genre)) &&
         config_setting_lookup_bstring(broadcastsetting, "url", &(radio_config->broadcast.url)),
-        "%s:%d - %s", config_error_file(cfg), config_error_line(cfg), config_error_text(cfg));
+        "Could not read broadcast settings");
 
   if (cfg != NULL) config_destroy(cfg);
   return radio_config;
