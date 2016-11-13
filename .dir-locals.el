4e1e787f88c2b1ec305155563f89d6a9082395e9
Guy John <guy@rumblesan.com>
2016-11-13 11:56:58 +0000

    Add dirlocals

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode . ((flycheck-clang-include-path . (
+                                            "./lib/include"
+                                            "./main/include"
+                                             )))))
