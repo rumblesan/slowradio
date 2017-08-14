9f125a091eb490e8b3d01ad04be4c627620dad4f
Guy John <guy@rumblesan.com>
2017-08-14 11:32:25 +0100

    Delete emacs dir-locals

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((c-mode . ((flycheck-clang-include-path . (
-                                            "./lib/include"
-                                            "./main/include"
-                                            "./tests/include"
-                                             )))))
